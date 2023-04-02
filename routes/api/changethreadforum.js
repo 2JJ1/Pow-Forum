f7344945d028cf4da634558a597670ad8f6181a8
2JJ1 <jonathonpo21@gmail.com>
2023-04-02 15:40:05 -0500

    Fixed change category

--- routes/api/changethreadforum.js
+++ routes/api/changethreadforum.js
@@ -19,12 +19,12 @@ router.post('/', async (req, res) => {
 		//Only allow logged in users to view profiles
 		if(!req.session.uid) throw 'You must be logged in'
 
-		let tid = parseInt(req.body.tid)
+		let tid = req.body.tid
 		if(!tid) throw "Thread ID not specified"
 		if(!Number.isInteger(tid)) throw "Invalid thread id"
 
 		let forum = req.body.forum
-		if(typeof forum !== "string") throw "Invalid forum"
+		if(typeof forum !== "number") throw "Invalid subcategory"
 
         //Get original poster's uid
         var thread = await Threads.findById(tid)
@@ -38,24 +38,23 @@ router.post('/', async (req, res) => {
 		let category = await forumapi.GetSubcategory(forum)
 
         //Check if the forum exists
-		if(!category) throw {safe: "Invalid category"};
+		if(!category) throw "Invalid category"
 
         //Check if client has access to the forum
 		let account = await accountAPI.fetchAccount(req.session.uid)
 		if(!forumapi.permissionsCheck(category.requiredRoles, account.roles)) throw {safe: 'No permission to post here'};
             
         //Update the thread's forum
-		await Threads.updateOne({_id: tid}, {forum})
-		await ThreadReplies.updateMany({tid}, {forum})
+		await Threads.updateOne({_id: tid}, {category: forum})
+		await ThreadReplies.updateMany({tid}, {category: forum})
 
         //No early exit, so must've passed
         response.success = true
 	} catch(e){
 		response.reason = "Server error"
-		if(e.safe && e.safe.length > 0)
-			response.reason = e.safe;
-		else
-			console.warn(e)
+		if (typeof e === 'string') response.reason = e
+		if(e.safe && e.safe.length > 0) response.reason = e.safe;
+		else console.warn(e)
 	}
 	
 	res.json(response)
