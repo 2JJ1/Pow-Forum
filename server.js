67fe052571a79f150849a87ec85992eeeae085a0
Yxild <122519357+Yxi1d@users.noreply.github.com>
2023-04-03 10:03:55 +1000

    Update server.js
    Formatted Everything to a cleaner style, added a better "console.log, console.error, console.warn" system using Winston 👍

--- server.js
+++ server.js
@@ -1,108 +1,136 @@
-require('dotenv').config()
-const express = require('express')
-var session = require('express-session')
-const MongoStore = require('connect-mongo')
-const webpush = require('web-push')
+require('dotenv').config();
+
+const Express = require('express'),
+    Session = require('express-session'),
+    MongoStore = require('connect-mongo'),
+    Webpush = require('web-push');
+
 //Open HTTP server
-const app = express()
-const mongoose = require('mongoose')
-const helmet = require("helmet")
-var compression = require('compression')
-const http = require('http').Server(app);
-const socketio = require("socket.io")
-const envfile = require('envfile')
-const fs = require('fs')
+const App = express(),
+    Mongoose = require('mongoose'),
+    Helmet = require("helmet"),
+    Compression = require('compression'),
+    HTTP = require('http').Server(app),
+    SocketIO = require("socket.io"),
+    Envfile = require('envfile'),
+    Fs = require('fs');
 
-const updateEnv = require('./my_modules/updateenv')
+const UpdateEnv = require('./my_modules/updateenv');
+const { CreateLogger, Format, Transports } = require("winston");
 
-if(!process.env.SUPPORT_EMAIL_ADDRESS) {
-	console.error("Missing support email address. Please run 'npm run setup'")
-	process.exit(2)
+/* 
+Added for better usage of Console.
+
+Logs information, Outputs into the Console! (it's cool & also outputs more information)
+
+    Usage:
+        logger.info(string: message);
+        logger.warn(string: message);
+        logger.error(string: message);
+*/
+
+const logger = createLogger({
+    level: 'info',
+    format: combine(
+        label({ label: 'SystemOS' }),
+        timestamp(),
+        printf(info => {
+            return `${info.timestamp} | [${info.label}] [${info.level.toUpperCase()}]: ${info.message}`;
+        })
+    ),
+    transports: [
+        new transports.Console()
+    ]
+});
+
+if (!process.env.SUPPORT_EMAIL_ADDRESS) {
+    logger.error("Missing support email address. Please run 'npm run setup'");
+    process.exit(2);
 }
 
-if(!process.env.FORUM_URL) {
-	console.error("Missing domain. Please run 'npm run setup'")
-	process.exit(2)
+if (!process.env.FORUM_URL) {
+    logger.error("Missing domain. Please run 'npm run setup'");
+    process.exit(2);
 }
 
-if(!process.env.MAILGUN_DOMAIN || !process.env.MAILGUN_APIKEY || !process.env.MAILGUN_NOREPLY_ADDRESS) {
-	console.error("Incomplete mailgun setup. Please run 'npm run setup'")
-	process.exit(2)
+if (!process.env.MAILGUN_DOMAIN || !process.env.MAILGUN_APIKEY || !process.env.MAILGUN_NOREPLY_ADDRESS) {
+    logger.error("Incomplete mailgun setup. Please run 'npm run setup'");
+    process.exit(2);
 }
 
 // MongoDB setup
 
 //Loads up all models
-require('./models')
-const ForumSettings = mongoose.model("ForumSettings")
+require('./models');
+const ForumSettings = mongoose.model("ForumSettings");
 
 //Database cleanup
-async function CleanMongoDatabase(){
-	//Deletes old active user counts
-	await mongoose.model("ActiveUsers").deleteMany({time: {$lt: Date.now() - 60000*15}})
+async function CleanMongoDatabase() {
+    //Deletes old active user counts
+    await Mongoose.model("ActiveUsers").deleteMany({ time: { $lt: Date.now() - 60000 * 15 } })
 
-	//Deletes forum audit logs older than 30 days
-	await mongoose.model("ForumAuditLogs").deleteMany({time: {$lt: Date.now() - 1000*60*60*24*30}})
+    //Deletes forum audit logs older than 30 days
+    await Mongoose.model("ForumAuditLogs").deleteMany({ time: { $lt: Date.now() - 1000 * 60 * 60 * 24 * 30 } })
 
-	//Deletes messages older than 14 days
-	await mongoose.model("Messages").deleteMany({time: {$lt: Date.now() - 1000*60*60*24*14}})
+    //Deletes messages older than 14 days
+    await Mongoose.model("Messages").deleteMany({ time: { $lt: Date.now() - 1000 * 60 * 60 * 24 * 14 } })
 }
 
 //Connect to database
-let mongoURL = `mongodb://localhost:27017/${process.env.DATABASE_NAME || "PFForum"}`
-mongoose.set('strictQuery', false)
-mongoose.connect(mongoURL)
-.then(async ()=> {
-	console.log("MongoDB database connected")
+let MongoURL = `mongodb://localhost:27017/${process.env.DATABASE_NAME || "PFForum"}`
+Mongoose.set('strictQuery', false)
+Mongoose.connect(MongoURL)
+    .then(async () => {
+        logger.info("MongoDB database connected")
 
-	//Automatic database setup for required documents or placeholder documents
-	{
-		let settings = await ForumSettings.find().lean()
+        //Automatic database setup for required documents or placeholder documents
+        {
+            let Settings = await ForumSettings.find().lean()
 
-		//Sets default title
-		if(!settings.find(setting => setting.type === "title")) {
-			await new ForumSettings({
-				type: "title",
-				value: "Pow Forums"
-			}).save()
-		}
+            //Sets default title
+            if (!Settings.find(setting => setting.type === "title")) {
+                await new ForumSettings({
+                    type: "title",
+                    value: "Pow Forums"
+                }).save()
+            }
 
-		//Sets default description
-		if(!settings.find(setting => setting.type === "description")) {
-			await new ForumSettings({
-				type: "description",
-				value: "An online community powered by Pow Forums"
-			}).save()
-		}
+            //Sets default description
+            if (!Settings.find(setting => setting.type === "description")) {
+                await new ForumSettings({
+                    type: "description",
+                    value: "An online community powered by Pow Forums"
+                }).save()
+            }
 
-		//Much of this stores into process.env so the process does not need to query the database everytime for highly reused data
+            //Much of this stores into process.env so the process does not need to query the database everytime for highly reused data
 
-		// Manages web-push configuration
-		//Generates push notification VAPID keys if the private or public vapid key is missing
-		if(!process.env.PRIVATE_VAPID_KEY || !process.env.PUBLIC_VAPID_KEY) {
-			let vapidKeys = webpush.generateVAPIDKeys()
+            // Manages web-push configuration
+            //Generates push notification VAPID keys if the private or public vapid key is missing
+            if (!process.env.PRIVATE_VAPID_KEY || !process.env.PUBLIC_VAPID_KEY) {
+                let VapidKeys = webpush.generateVAPIDKeys()
 
-			updateEnv({
-				PRIVATE_VAPID_KEY: vapidKeys.privateKey,
-				PUBLIC_VAPID_KEY: vapidKeys.publicKey,
-			})
-		}
-		webpush.setVapidDetails(`mailto:${process.env.SUPPORT_EMAIL_ADDRESS}`, process.env.PUBLIC_VAPID_KEY, process.env.PRIVATE_VAPID_KEY);
-	}
+                UpdateEnv({
+                    PRIVATE_VAPID_KEY: VapidKeys.privateKey,
+                    PUBLIC_VAPID_KEY: VapidKeys.publicKey,
+                })
+            }
+            webpush.setVapidDetails(`mailto:${process.env.SUPPORT_EMAIL_ADDRESS}`, process.env.PUBLIC_VAPID_KEY, process.env.PRIVATE_VAPID_KEY);
+        }
 
-	//Clean database on every launch
-	CleanMongoDatabase()
-	//Clean database every 24 hours
-	setInterval(CleanMongoDatabase, 1000 * 60 * 60 * 24)
-})
+        //Clean database on every launch
+        CleanMongoDatabase()
+        //Clean database every 24 hours
+        setInterval(CleanMongoDatabase, 1000 * 60 * 60 * 24)
+    })
 
 // Express.js configuration
 
 //Helps protect from some well-known web vulnerabilities by setting HTTP headers appropriately.
-app.use(helmet())
+app.use(Helmet())
 
 //Compress all responses
-app.use(compression())
+app.use(Compression())
 
 //Trust first proxy
 app.set('trust proxy', 1)
@@ -122,45 +150,45 @@ app.use(express.static('public', { extensions: ['html'] }))
 // Login session initialization
 
 //Generate session secret if one does not exist
-if(!process.env.SESSION_SECRET) {
-	let result = '';
-	let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	let charactersLength = characters.length;
-	let counter = 0;
-	while (counter < 20) {
-		result += characters.charAt(Math.floor(Math.random() * charactersLength));
-		counter += 1;
-	}
+if (!process.env.SESSION_SECRET) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    let counter = 0;
+    while (counter < 20) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
 
-	let parsedEnv = envfile.parse(fs.readFileSync('.env', "utf8"))
-	parsedEnv.SESSION_SECRET = result
-	fs.writeFileSync('.env', envfile.stringify(parsedEnv))
+    let parsedEnv = envfile.parse(fs.readFileSync('.env', "utf8"))
+    parsedEnv.SESSION_SECRET = result
+    fs.writeFileSync('.env', envfile.stringify(parsedEnv))
 
-	process.env.SESSION_SECRET = result
+    process.env.SESSION_SECRET = result
 }
 
 //Express routes will get sessions through session
-let sessionConf = {
-	secret: process.env.SESSION_SECRET,
-	name: process.env.SESSION_COOKIE_NAME || '_PFSec',
-	store: MongoStore.create({
-		mongoUrl: mongoURL,
-		stringify: false,
-	}),
-	saveUninitialized: false, //Prevents every single request from being recognized as a session
-	rolling: true, //Resets expiration date
-	resave: true, //Resaves cookie on server. Necessary because of the expiration date being reset
-	cookie: { 
-		httpOnly: process.env.NODE_ENV !== 'development' && new URL(process.env.FORUM_URL).hostname !== "localhost",
-		secure: process.env.NODE_ENV !== 'development' && new URL(process.env.FORUM_URL).hostname !== "localhost",
-		maxAge: 1000*60*60*24*365 // (1 week in milliseconds) 1 second -> 1 minute -> 1 hour -> 1 day -> 1 year
-	}
+let SessionConf = {
+    secret: process.env.SESSION_SECRET,
+    name: process.env.SESSION_COOKIE_NAME || '_PFSec',
+    store: MongoStore.create({
+        mongoUrl: mongoURL,
+        stringify: false,
+    }),
+    saveUninitialized: false, //Prevents every single request from being recognized as a session
+    rolling: true, //Resets expiration date
+    resave: true, //Resaves cookie on server. Necessary because of the expiration date being reset
+    cookie: {
+        httpOnly: process.env.NODE_ENV !== 'development' && new URL(process.env.FORUM_URL).hostname !== "localhost",
+        secure: process.env.NODE_ENV !== 'development' && new URL(process.env.FORUM_URL).hostname !== "localhost",
+        maxAge: 1000 * 60 * 60 * 24 * 365 // (1 week in milliseconds) 1 second -> 1 minute -> 1 hour -> 1 day -> 1 year
+    }
 }
-if(process.env.COOKIE_DOMAIN) sessionConf.cookie.domain = process.env.COOKIE_DOMAIN
-let sessionMiddleware = session(sessionConf)
+if (process.env.COOKIE_DOMAIN) SessionConf.cookie.domain = process.env.COOKIE_DOMAIN
+let SessionMiddleware = session(SessionConf)
 
 //Express server use session 
-app.use(sessionMiddleware)
+app.use(SessionMiddleware)
 
 // Define what HTTP routes to listen for
 
@@ -171,39 +199,39 @@ app.use('/api/', require('./routes/api/router'))
 app.use("/", require('./routes/www/router'))
 
 //No route matched? Default route -> Send 404 page
-app.use(function(req, res, next){
-	res.status(404).render("404")
+app.use(function (req, res, next) {
+    res.status(404).render("404")
 })
 
 //Express.js exception handling
-app.use(function(err, req, res, next) {
-	try {
-		if (typeof err === "string") return res.status(400).render("400", {reason: err})
-		else if(err.name === "URIError") return res.status(400).render("400", {reason: "Bad request: Invalid URI"})
-		else{
-			var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-			console.log("Express.js error:", err, `. For URL: ${fullUrl}`)
-			return res.status(400).send("The server has errored... This will be fixed when the admins have noticed");
-		}
-	}
-	catch(e){
-		console.log("Exception handler just errored: ", e)
-	}
+app.use(function (Err, Req, Res, Next) {
+    try {
+        if (typeof Err === "string") return Res.status(400).render("400", { reason: Err })
+        else if (Err.name === "URIError") return Res.status(400).render("400", { reason: "Bad request: Invalid URI" })
+        else {
+            var FullUrl = Req.protocol + '://' + Req.get('host') + Req.originalUrl;
+            logger.warn(`Express.js error: ${Err}, URL errored on: ${FullUrl}`)
+            return Res.status(400).send("The server has errored, This will be fixed when the admins have noticed.");
+        }
+    }
+    catch (e) {
+        logger.info("Exception handler just errored: ", e)
+    }
 })
 
 //Starts HTTP server
-http.listen(process.env.PORT || 8087, () => {
-	console.log(`Pow Forum server started on http://localhost:${process.env.PORT || 8087}`)
+HTTP.listen(process.env.PORT || 8087, () => {
+    logger.info(`Pow Forum server started on http://localhost:${process.env.PORT || 8087}`)
 })
 
 // Socket.io configuration
 
 //Create Socket.io server
-const io = new socketio.Server(http)
-module.exports.io = io
+const IO = new socketio.Server(http)
+module.exports.io = IO
 
 //Websocket server use session
-io.engine.use(sessionMiddleware)
+IO.engine.use(SessionMiddleware)
 
 //Listen to websocket requests
-io.on('connection', require('./my_modules/websocket'))
+IO.on('connection', require('./my_modules/websocket'))
