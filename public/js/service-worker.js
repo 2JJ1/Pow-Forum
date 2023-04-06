be664d896fce9127adab0f532cd105a0a37b93cf
2JJ1 <jonathonpo21@gmail.com>
2023-04-06 13:31:55 -0500

    Cleanup

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,20 +1,15 @@
 self.addEventListener('push', function(event) {
-	console.log('Push received:', event.data.text())
-  if (event.data) {
-   var pushedMessage = JSON.parse(event.data.text()); //EX: {"type":"notification","title":"title!", "options":{ "body" : "body info...": "./img.png" }}
-	if(pushedMessage.type == "notification"){		
-		let promiseChain = self.registration.showNotification(pushedMessage.title, pushedMessage.options);
-		
-		event.waitUntil(promiseChain);
+	if (event.data) {
+		var pushedMessage = JSON.parse(event.data.text())
+		if(pushedMessage.type == "notification"){		
+			let promiseChain = self.registration.showNotification(pushedMessage.title, pushedMessage.options);
+			event.waitUntil(promiseChain);
+		}
 	}
-  } else {
-    console.log('This push event has no data.');
-  }
-});
+})
 
 self.addEventListener('notificationclick', function(event) {
-    console.log('rekt', event)
-    event.notification.close(); // Android needs explicit close.
+    event.notification.close() // Android needs explicit close.
     event.waitUntil(
         clients.matchAll({type: 'window'}).then( windowClients => {
             // Check if there is already a window/tab open with the target URL
