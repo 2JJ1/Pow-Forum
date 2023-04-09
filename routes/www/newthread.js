686bceb44712d0ae95f675c67f52a3d5ac7016a0
2JJ1 <jonathonpo21@gmail.com>
2023-04-09 16:24:26 -0500

    Pointless error handling

--- routes/www/newthread.js
+++ routes/www/newthread.js
@@ -8,42 +8,32 @@ const accountAPI = require('../../my_modules/accountapi')
 const Subcategories = mongoose.model("Subcategories")
 
 router.get('/:forum/newthread', async (req, res, next) => {
-	try {
-        let pagedata = {
-		    powForum: req.powForum,
-            accInfo: req.account,
-        }
+    let pagedata = {
+        powForum: req.powForum,
+        accInfo: req.account,
+    }
 
-        let subcategory = parseInt(req.params.forum)
-		if(!Number.isInteger(subcategory)) return next("Invalid subcategory")
+    let subcategory = parseInt(req.params.forum)
+    if(!Number.isInteger(subcategory)) return next("Invalid subcategory")
 
-        //Only logged in users can create threads
-        if(!req.session.uid) return res.status(400).render("400", {reason: "Please login to create a thread"})
+    //Only logged in users can create threads
+    if(!req.session.uid) return res.status(400).render("400", {reason: "Please login to create a thread"})
 
-        //Check if the account is pending an email verification
-        if(!await accountAPI.emailVerified(req.session.uid)) return res.status(400).render("400", {reason: "Please verify your email to create a thread"})
+    //Check if the account is pending an email verification
+    if(!await accountAPI.emailVerified(req.session.uid)) return res.status(400).render("400", {reason: "Please verify your email to create a thread"})
 
-        //Check if the category exists
-        if(!await Subcategories.findById(req.params.forum)) return res.status(400).render("404", {reason: "Category does not exist"})
+    //Check if the category exists
+    if(!await Subcategories.findById(req.params.forum)) return res.status(400).render("404", {reason: "Category does not exist"})
 
-    	pagedata.forumData = {
-            name: req.params.forum,
-            category: await forumapi.GetSubcategory(subcategory),
-        }
+    pagedata.forumData = {
+        name: req.params.forum,
+        category: await forumapi.GetSubcategory(subcategory),
+    }
 
-        //Check if client can post here
-        if(!forumapi.permissionsCheck(pagedata.forumData.category.requiredRoles, pagedata.accInfo.roles)) return res.status(400).render('400', {reason: "You lack permissions to post threads in this category"})
-		
-		res.render('pages//newthread', pagedata);
-	} catch(e){
-		let reason = "Server error"
-		if(e.safe && e.safe.length > 0)
-			reason = e.safe;
-		else
-			console.warn(e)
-
-		res.status(200).send(reason)
-	}
+    //Check if client can post here
+    if(!forumapi.permissionsCheck(pagedata.forumData.category.requiredRoles, pagedata.accInfo.roles)) return res.status(400).render('400', {reason: "You lack permissions to post threads in this category"})
+    
+    res.render('pages//newthread', pagedata)
 });
 
 module.exports = router;
