405887dd42d98fa86b9ccbb8a39a5430d1a24a49
2JJ1 <jonathonpo21@gmail.com>
2025-05-06 08:01:30 -0500

    Fixed delete subcategory

--- routes/api/dashboard/categories/deletesubcategory.js
+++ routes/api/dashboard/categories/deletesubcategory.js
@@ -5,6 +5,7 @@ const Subcategories = mongoose.model("Subcategories")
 const Threads = mongoose.model("Threads")
 const ThreadReplies = mongoose.model("ThreadReplies")
 const ForumAuditLogs = mongoose.model("ForumAuditLogs")
+const ThreadReplyReacts = mongoose.model("ThreadReplyReacts")
 
 const accountAPI = require('../../../../my_modules/accountapi')
 
@@ -14,21 +15,28 @@ router.delete("/deletesubcategory", async (req, res, next) => {
 	try{
         let response = {success: false}
 
-        let {id, category, password} = req.body
-        if(!category || !password) throw "Invalid request"
+        let {id, password} = req.body
+        if(!id || !password) throw "Invalid request"
         id = parseInt(id)
 
         //First validate password
         if(!await accountAPI.CheckPassword(req.session.uid, password)) throw "Incorrect password"
 
-        //Delete the category
-        let subcategory = await Subcategories.findById(id)
-        subcategory.deleteOne()
+        //Delete the subcategory
+        let subcategory = await Subcategories.findOneAndDelete({_id: id})
+
+        //Delete data related to thread replies
+        const cursor = Threads.find({category: id}).cursor()
+        for await (const thread of cursor) {
+            let replies = await ThreadReplies.find({tid: thread._id})
+            for (const reply of replies) {
+                await ThreadReplyReacts.deleteMany({trid: replies._id})
+            }
+        }
 
         //Delete related threads
         await Threads.deleteMany({category: id})
         await ThreadReplies.deleteMany({category: id})
-        //TODO- Delete info related to thread replies(reacts...)
         
 		//Code hasn't exited, so assume success
 		response.success = true
@@ -37,7 +45,7 @@ router.delete("/deletesubcategory", async (req, res, next) => {
         //Log audit
 		new ForumAuditLogs({
             time: Date.now(),
-            type: "Delete category",
+            type: "Delete subcategory",
             byUID: req.session.uid,
             content: {
                 name: subcategory.name,
